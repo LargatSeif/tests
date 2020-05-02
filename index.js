c70af9be283d2669aa51d7fe1c086d9993310b6f
LargatSeif <largatseif@outlook.com>
2020-05-02 03:15:57 +0200

    1 file modified, 1 added, 1 deleted

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 // Import stylesheets
-import './style.css';
-
+import "./style.css";
+import "./logo.png";
 // Write Javascript code!
-const appDiv = document.getElementById('app');
+const appDiv = document.getElementById("app");
 appDiv.innerHTML = `<h1>JS Starter</h1>`;
+
+var fs = require("fs");
+function base64_encode(file) {
+  // read binary data
+  var bitmap = fs.readFileSync(file);
+  // convert binary data to base64 encoded string
+  return new Buffer(bitmap).toString("base64");
+}
+
+console.log(base64_encode("./logo.png"));
